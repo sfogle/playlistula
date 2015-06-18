aa7c0b936daa502e7c3a3dbad402b39497dd1050
Sean Fogle <sean.fogle@gmail.com>
2015-06-17 20:37:03 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='playlistula',
+      version='0.1',
+      description='Play on, playa',
+      url='http://github.com/sfogle/playlistula',
+      author='Sean Fogle',
+      author_email='sean.fogle@gmail.com',
+      license='GPL v2',
+      install_requires=[
+          'requests',
+      ])
